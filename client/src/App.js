8853391b6919cf9b02c55cd321f0b580dc82468a
ditya17 <nitya17nitya@gmail.com>
2023-09-13 12:01:07 +0530

    check_tailwind_css

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import 'tailwindcss/base'
+
+function App() {
+  return (
+    <div className="bg-blue-200 p-8 text-center">
+      <h1 className="text-3xl font-bold mb-4 text-blue-800">
+        Welcome to My Tailwind React App
+      </h1>
+    </div>
+  );
+}
+
+export default App;
