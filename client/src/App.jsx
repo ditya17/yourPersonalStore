a9dc8ea1fc2ff51809d46ee362cb4ce33b26f64a
ditya17 <nitya17nitya@gmail.com>
2023-09-14 22:51:04 +0530

    fixing merge

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,3 @@
-import { Route, Routes } from "react-router-dom";
-import HomePage from "./pages/home/HomePage";
-import AboutPage from "./pages/AboutPage";
-
-function App() {
-  return (
-    <div className="font-DM_Sans">
-      <Routes>
-        <Route index path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-      </Routes>
-    </div>
 import Homeee from "./Homeee";
 import SignInPage from "./SignInPage";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -7,9 +7,6 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
   <React.StrictMode>
     <GoogleOAuthProvider clientId="101961828065-ge9ktthv316d67mdrrbtc47c866dp7i4.apps.googleusercontent.com">
 
